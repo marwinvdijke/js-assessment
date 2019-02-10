8fa2239c7c175c947b1c1a1dec254ddf47d12cd3
MarwinvDijke <45556864+marwinvdijke@users.noreply.github.com>
2019-02-10 18:33:03 +0100

    My Version

--- app/arrays.js
+++ app/arrays.js
@@ -17,32 +17,39 @@ exports.arraysAnswers = {
 
   },
 
-  append: function(arr, item) {
-
+   append: function(arr, item) {
+    arr.push(item);
+    return arr;
   },
 
-  truncate: function(arr) {
 
+  truncate: function(arr) {
+   arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(10);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    arr.pop();
+    return arr;
   },
 
   duplicates: function(arr) {
